73412baffd3c3c7911dde6f41ac6abf904f54dc0
ConceicaoLourenco <conceicaolourenzo@gmail.com>
2019-06-24 21:53:23 -0300

    criando componentes Login e cadastro de Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React , {Component} from 'react';
 
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
-import './App.css';
+ 
 
  import NavBar from './components/layout/Navbar'
+ import SingIn from './components/auth/SingIn'
+ import SingUp from './components/auth/SingUp'
  import Dashboard from './components/dashboard/Dashboard'
  import ProjectDetails from './components/projects/ProjectDetails'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-         <NavBar />
-         <Switch>
-             <Route exact path='/' component={Dashboard} />
-             <Route path='/project/:id' component={ProjectDetails} />
-         </Switch>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+      return (
+        <BrowserRouter>
+          <div>
+            <NavBar />
+            <Switch>
+                <Route exact path='/' component={Dashboard} />
+                <Route path='/project/:id' component={ProjectDetails} />
+                <Route path='/singin' component={SingIn} />
+                <Route path='/singup' component={SingUp} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      );
+  }
 }
 
 export default App;
