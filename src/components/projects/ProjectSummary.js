c8126335e4aa703f9d89aaa5c1782d739d8f1ee4
ConceicaoLourenco <conceicaolourenzo@gmail.com>
2019-06-26 23:07:18 -0300

    iniciando os Reducers

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return ( 
             <div className="card z-depth-0 project-summary"> 
                     <div className="card-content grey-text text-dark"> 
-                        <span className="card-title"> Project Title  </span>
+                        <span className="card-title"> {project.tile} </span>
                         <p> This is the project </p>
                         <p className="grey-text">  20 June , 2Am  </p>
                     </div> 
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,15 @@
+const initState = {
+
+    projects: [
+        {id: '1', title : 'Maratona code in house', content:'blah blah  blah  blah blah blah blah blah blah '},
+        {id: '2', title : 'Coding your app lous', content:'blah blah  blah  blah blah blah blah blah blah '},
+        {id: '3', title : 'Create your newpapers in app lous', content:'blah blah  blah  blah blah blah blah blah blah '},
+    ]
+} 
+
+const projectReducer = (state = initState, action) => {
+
+    return state
+}
+
+export default projectReducer
