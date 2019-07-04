5141113e74e5793d0f87ae236fd915edf2beecd4
ConceicaoLourenco <conceicaolourenzo@gmail.com>
2019-07-03 23:34:48 -0300

    adicionando e listando projeto firebase

--- src/index.js
+++ src/index.js
@@ -2,24 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Provider} from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import {createStore, applyMiddleware, compose} from 'redux';
-import rootReducer from './store/reducers/rootReducer';
-import * as serviceWorker from './serviceWorker';
-
-import {reduxFirestore, getFirestore} from 'redux-firestore';
-import {reactReduxFirebase, getFirebase} from 'react-redux-firebase';
-
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig'
 
 const store = createStore(rootReducer,
-    compose(
-        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-        reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig)
-        )
-    );
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(fbConfig), // redux binding for firebase
+    reduxFirestore(fbConfig) // redux bindings for firestore
+  )
+);
 
-ReactDOM.render(<Provider store={store}><App /></Provider> , document.getElementById('root')); 
-serviceWorker.unregister();
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker(); 
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,11 +1,4 @@
-const initState = {
-
-    projects: [
-        {id: '1', title : 'Maratona code in house', content:' This is my routinne , i am great progress in react '},
-        {id: '2', title : 'Coding your app lous', content:'Today i am go coding my future app'},
-        {id: '3', title : 'Create your newpapers in app lous', content:'I Need publish more news about my app '},
-    ]
-} 
+const initState = { } 
 
 const projectReducer = (state = initState, action) => {
 
@@ -16,8 +9,7 @@ const projectReducer = (state = initState, action) => {
         case 'CREATE_PROJECT_ERROR':
             console.log('CREATE_PROJECT ERROR --- ', action.err)
             return state;
-        default:
-            console.log('Default Case  --- ', action.project)
+        default: 
             return state;
     } 
 }
